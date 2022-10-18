d1ab703d608764d4be01005bf6244d41cea7612b
Joel Watkins <eck087@yahoo.co.uk>
2022-10-18 16:31:24 +0100

    added list of games to each card

--- src/index.js
+++ src/index.js
@@ -1,52 +1,65 @@
-
 console.log(data);
 
-//You can start simple and just render a single 
+//You can start simple and just render a single
 //pokemon card from the first element
 console.log(data[0]);
 
-function capitalize (string) {
-    return string.charAt(0).toUpperCase() + string.slice(1)
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function createCard (liCard) {
-    const ul = document.querySelector('ul')
-    liCard.setAttribute('class', 'card')
-    ul.appendChild(liCard)
+function createCard(liCard) {
+  const ul = document.querySelector("ul");
+  liCard.setAttribute("class", "card");
+  ul.appendChild(liCard);
 }
 
-function createTitle (i, liCard) {
-    console.log(`Inside createTitle fuction, i is currently ${i}`)
-    const title = document.createElement('h2')
-    title.innerText = (capitalize(data[i].name))
-    title.classList.add('card--title')
-    liCard.appendChild(title)
+function createTitle(pokemonCard, liCard) {
+  const title = document.createElement("h2");
+  title.innerText = capitalize(pokemonCard.name);
+  title.classList.add("card--title");
+  liCard.appendChild(title);
 }
 
-function createImage (i, liCard) {
-    const image = document.createElement('img')
-    image.src = data[i].sprites.other['official-artwork'].front_default    
-    image.setAttribute('width', '256')
-    image.classList.add('card--img')
-    liCard.appendChild(image)
+function createImage(pokemonCard, liCard) {
+  const image = document.createElement("img");
+  image.src = pokemonCard.sprites.other["official-artwork"].front_default;
+  image.setAttribute("width", "256");
+  image.classList.add("card--img");
+  liCard.appendChild(image);
 }
 
-function createCardText (i, liCard) {
-    const ul = document.createElement('ul')
-    ul.classList.add('card--text')
-    data[i].stats.forEach(item => {
-        const liStat = document.createElement('li')
-        liStat.innerText = `${item.stat.name.toUpperCase()}: ${item.base_stat}`
-        ul.appendChild(liStat)
-    })
-    liCard.appendChild(ul)
+function createCardText(pokemonCard, liCard) {
+  const ul = document.createElement("ul");
+  ul.classList.add("card--text");
+  pokemonCard.stats.forEach((item) => {
+    const liStat = document.createElement("li");
+    liStat.innerText = `${item.stat.name.toUpperCase()}: ${item.base_stat}`;
+    ul.appendChild(liStat);
+  });
+  liCard.appendChild(ul);
 }
 
-data.forEach((pokemonCard, i) => {
-    const liCard = document.createElement('li')
-    createCard (liCard)
-    createTitle(i, liCard)
-    createImage(i, liCard)
-    createCardText(i, liCard)
-})
+function createGameList(pokemonCard, liCard) {
+  const ul = document.createElement("ul");
+  ul.classList.add("card--text");
+  const liGames = document.createElement("li");
+  liGames.textContent = "APPEARS IN: ";
 
+  pokemonCard.game_indices.forEach((gameIndex) => {
+    liGames.textContent += `${gameIndex.version.name}, `;
+    ul.appendChild(liGames);
+  });
+  liCard.appendChild(ul);
+}
+
+data.forEach((pokemonCard) => {
+  const liCard = document.createElement("li");
+  createCard(liCard);
+  createTitle(pokemonCard, liCard);
+  createImage(pokemonCard, liCard);
+  createCardText(pokemonCard, liCard);
+  createGameList(pokemonCard, liCard);
+});
+
+liCard.addEventListener("click", () => {});
